8991b7a5e5802685d986943fe9adab8e127a030c
Hatzen <kaihartz@gmx.net>
2022-03-26 16:55:46 +0000

    Deploying to release-page from @ Hatzen/OnPinion@d3f8fa305dd197a308bba15ff832ca1f3be78f14 🚀

---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/app'
+import { createTheme , ThemeProvider } from '@mui/material'
+
+const theme = createTheme ({
+    palette: {
+        primary: {
+            main: '#E3E6E8', // Gray
+        },
+        secondary: {
+            main: '#004A7F', // Blue
+        }
+    }
+})
+
+ReactDOM.render(
+    <ThemeProvider theme={theme}>
+        <App/>
+    </ThemeProvider>,
+    document.getElementById('app-root'),
+)
+
+// Check config is working for observable non instantiated attributes.
+// https://mobx.js.org/installation.html#installation
+// eslint-disable-next-line no-prototype-builtins
+if (!new class { x: any }().hasOwnProperty('x')) throw new Error('Transpiler is not configured correctly to set defaults for props.')
---
+++ src/services/firebase.service.ts
@@ -0,0 +1,71 @@
+// import firebaseui from 'firebaseui'
+// https://firebase.google.com/docs/web/setup?authuser=1&hl=de
+import { FirebaseApp, initializeApp } from 'firebase/app'
+import { getAnalytics, Analytics } from 'firebase/analytics'
+import { Database, DatabaseReference, getDatabase, onValue, ref, set } from 'firebase/database'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+export class FirebaseService {
+
+    private app!: FirebaseApp
+    private analytics!: Analytics
+    private database!: Database
+    
+    
+    initFirebase (): void {
+        this.app = initializeApp(firebaseConfig)
+        this.analytics = getAnalytics(this.app)
+        this.database = getDatabase(this.app)
+
+        /*
+        // https://firebase.google.com/docs/web/learn-more?authuser=1&hl=de#config-object
+        https://firebase.google.com/docs/auth/web/firebaseui?hl=de&authuser=1#set_up_sign-in_methods
+
+        TODO: Why is auth not defined
+        const ui = new firebaseui.auth.AuthUI(firebase.auth())
+        ui.start('#firebaseui-auth-container', {
+            signInOptions: [
+                {
+                    provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+                    requireDisplayName: false
+                }
+            ]
+        })
+        */
+    }
+
+    // https://firebase.google.com/docs/database/web/read-and-write
+    listenData(callback: (count: number) => void): void {
+        onValue(this.ref, (snapshot) => {
+            const data = snapshot.val() // TODO: Add soem typings
+            callback(data)
+        })
+    }
+
+    updateData(count: number): void {
+        set(this.ref, Math.random()*12 + count)
+    }
+
+    get ref(): DatabaseReference {
+        return ref(this.database, 'fourenergy/surveys/first/surveyentrys/homeoffice/votes')
+    }
+
+}
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// https://firebase.google.com/docs/web/learn-more?authuser=1&hl=de#config-object
+const firebaseConfig = {
+    apiKey: 'AIzaSyBJA_OP8pNeKS8UOfhAcOpUskkHRa-V0bM', // TODO: Hide? https://stackoverflow.com/a/37484053/8524651
+    authDomain: 'onpinion-d2fd9.firebaseapp.com',
+    databaseURL: 'https://onpinion-d2fd9-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'onpinion-d2fd9',
+    storageBucket: 'onpinion-d2fd9.appspot.com',
+    messagingSenderId: '666898493007',
+    appId: '1:666898493007:web:754f8e9cbad09439ad843a',
+    measurementId: 'G-FC0RQS22SB'
+}

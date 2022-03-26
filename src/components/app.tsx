c5228ca33e1c5307f443dc37499702186349cadf
Hatzen <kaihartz@gmx.net>
2022-03-26 18:53:40 +0100

    init startpage

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -2,9 +2,8 @@ import React from 'react'
 import { Provider } from 'mobx-react'
 import UiStore from '../stores/uiStore'
 import { StoreProps } from '../stores/storeHelper'
-import { FirebaseService } from '../services/firebase.service'
-import { ResponsiveBar } from '@nivo/bar'
-import { Button } from '@mui/material'
+import { Button, Input } from '@mui/material'
+import '../styles/app.css'
 
 
 interface AppState {
@@ -13,19 +12,12 @@ interface AppState {
 
 class App extends React.Component<any, AppState> {
 
-    private firebaseService: FirebaseService
     
     constructor(props: any) {
         super(props)
         this.state ={
             count: 77,
         }
-        this.firebaseService = new FirebaseService()
-        this.firebaseService.initFirebase()
-        this.firebaseService.listenData((count: number) => {
-            // TODO: Remove alert('hello ' + count)
-            this.setState({count: count})
-        })
     }
 
     render(): JSX.Element {
@@ -33,84 +25,22 @@ class App extends React.Component<any, AppState> {
             uiStore: UiStore
         }
         return (
-            <div style={{ position: 'absolute', top:0, left:0, bottom: 0, right:0 }}>
-                {this.bar}
+            <div style={{ position: 'absolute', top:0, left:0, bottom: 0, right:0, background: 'url(logo.png) no-repeat', backgroundPosition: 'center', backgroundSize: 'contain' }}>
                 <Provider {...stores}>
-                    <div>
-                        Home
-                        <Button
-                            onClick={() => {
-                                this.firebaseService.updateData(5)
-                            }}
-                            variant="contained">Update Value</Button>
+                    <Button style={{float: 'right'}} variant="text">
+                            Umfrage erstellen
+                    </Button>
+                    <div style={{textAlign: 'center', margin: 'calc(50% - 120px) auto 0 auto'}}>
+                        <h1 className='color-primary'>OnPinion</h1>
+                        <p>Bitte geben sie den Code für die Umfrage ein:</p>
+                        <Input></Input>
+                        <Button style={{marginLeft: '25px'}} variant="contained">
+                            Zur Umfrage
+                        </Button>
                     </div>
                 </Provider>
             </div>
         )
-    }
-    
-    get barData(): any {
-        return [
-            {
-                'answer': 'yes',
-                'Homeoffice': this.state.count
-            }
-        ]
-    }
-
-    get bar(): JSX.Element {
-        // https://nivo.rocks/bar/
-        return <ResponsiveBar
-            data={this.barData}
-            keys={[
-                'Homeoffice'
-            ]}
-            indexBy="answer"
-            margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
-            padding={0.3}
-            valueScale={{ type: 'linear' }}
-            indexScale={{ type: 'band', round: true }}
-            colors={{ scheme: 'nivo' }}
-            borderColor={{
-                from: 'color',
-                modifiers: [
-                    [
-                        'darker',
-                        1.6
-                    ]
-                ]
-            }}
-            axisTop={null}
-            axisRight={null}
-            axisBottom={{
-                tickSize: 5,
-                tickPadding: 5,
-                tickRotation: 0,
-                legend: 'answer',
-                legendPosition: 'middle',
-                legendOffset: 32
-            }}
-            axisLeft={{
-                tickSize: 5,
-                tickPadding: 5,
-                tickRotation: 0,
-                legend: 'count',
-                legendPosition: 'middle',
-                legendOffset: -40
-            }}
-            labelSkipWidth={12}
-            labelSkipHeight={12}
-            labelTextColor={{
-                from: 'color',
-                modifiers: [
-                    [
-                        'darker',
-                        1.6
-                    ]
-                ]
-            }}
-            role="application"
-        />
     }
 }
 

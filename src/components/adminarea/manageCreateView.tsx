80085e6c10f2977a17730de0b4ca98d14e04c235
Hatzen <kaihartz@gmx.net>
2022-04-05 22:59:53 +0200

    some dummies for create survey view

--- src/components/adminarea/manageCreateView.tsx
+++ src/components/adminarea/manageCreateView.tsx
@@ -1,10 +1,57 @@
 import * as React from 'react'
-import Card from '@mui/material/Card'
+import { Button, Divider, List, ListItem, ListItemText, Skeleton, Snackbar } from '@mui/material'
 
 export const ManageCreateView = (): JSX.Element => {
+    const [open, setOpen] = React.useState(false)
+
+    const handleClick = (): void => {
+        setOpen(true)
+    }
+  
+    const handleClose = (event: any): any => {
+        if (event.reason === 'clickaway') {
+            return
+        }
+    
+        setOpen(false)
+    }
+  
+    const action = (
+        <React.Fragment>
+            <Button color='secondary' size='small' onClick={handleClose}>
+                Close
+            </Button>
+        </React.Fragment>
+    )
     return (
-        <Card sx={{ maxWidth: 345 }}>
-        </Card>
+        <div>
+            <Skeleton sx={{ height: 190 }} animation='wave' variant='rectangular' />
+            <List component='nav' aria-label='mailbox folders'>
+                <ListItem button>
+                    <ListItemText primary='Inbox' />
+                </ListItem>
+                <Divider />
+                <ListItem button divider>
+                    <ListItemText primary='Drafts' />
+                </ListItem>
+                <ListItem button>
+                    <ListItemText primary='Trash' />
+                </ListItem>
+                <Divider light />
+                <ListItem button>
+                    <ListItemText primary='Spam' />
+                </ListItem>
+            </List>
+            
+            <Button onClick={handleClick}>Open simple snackbar</Button>
+            <Snackbar
+                open={open}
+                autoHideDuration={6000}
+                onClose={handleClose}
+                message='Note archived'
+                action={action}
+            />
+        </div>
     )
 }
 

1276c1519997dac811765f5d723fd753e2ca0b06
Hatzen <kaihartz@gmx.net>
2022-04-10 07:47:32 +0000

    Deploying to release-page from @ Hatzen/OnPinion@c336fef0c01a84c6ae2448bb03fd20a9a58f7cd8 🚀

--- src/components/adminarea.tsx
+++ src/components/adminarea.tsx
@@ -1,33 +1,22 @@
 import { Grid } from '@mui/material'
 import React from 'react'
+import { Survey } from '../model/survey'
+import { FirebaseService } from '../services/firebase.service'
 import AddCard from './adminarea/addCard'
 import SurveyCard from './adminarea/surveyCard'
 import { NavigationProps } from './app'
 
 const AdminArea = (props: NavigationProps): JSX.Element => {
-    const surveyData: SurveyEntry[] = [
-        {
-            id: 'asjaaknaflaxxvhwekspsjhfmclk',
-            name: 'Zufriedenheit',
-            participationcount: 6
-        },
-        {
-            id: 'asjaak1xvhweks5psjhfmclk',
-            name: 'News@4E',
-            participationcount: 124
-        },
-        {
-            id: 'xgraxxvhwekspsjhfmclk',
-            name: 'Mitarbeiter Gehaltsverhandlung',
-            participationcount: 0
-        }
-    ]
+    const [surveys, setSurveys] = React.useState(new Array<Survey>())
 
+    const service = new FirebaseService()
+    service.initFirebase()
+    service.getSurveys().then(setSurveys)
 
     return (
         <div>
             <Grid container spacing={{ xs: 2, md: 2 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                {surveyData.map((element, index) => (
+                {surveys.map((element, index) => (
                     <Grid item xs={2} sm={2} md={2} key={index}>
                         <SurveyCard entry={element} navigate={props.navigate}/>
                     </Grid>
--- src/components/adminarea/surveyCard.tsx
+++ src/components/adminarea/surveyCard.tsx
@@ -4,20 +4,24 @@ import CardContent from '@mui/material/CardContent'
 import Typography from '@mui/material/Typography'
 import { CardActionArea } from '@mui/material'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { SurveyEntry } from '../adminarea'
 import { NavigationProps } from '../app'
+import { Survey } from '../../model/survey'
 
 class SurveyCardProps {
-    entry!: SurveyEntry
+    entry!: Survey
 }
 
 export const SurveyCard = (props: SurveyCardProps & NavigationProps): JSX.Element => {
+    const stateText = props.entry.isClosed ? 'Closed' : 'Open'
     return (
         <Card sx={{ maxWidth: 345 }}>
-            <CardActionArea onClick={() => { props.navigate('result/' + props.entry.id) }}>
+            <CardActionArea onClick={() => { props.navigate('manage/result/' + props.entry.id) }}>
                 <CardContent>
                     <Typography gutterBottom variant="h5">
-                        {props.entry.participationcount} <FontAwesomeIcon icon={'user-check'} />
+                        {props.entry.participations} <FontAwesomeIcon icon={'user-check'} />
+                    </Typography>
+                    <Typography gutterBottom variant="h5">
+                        {stateText}
                     </Typography>
                     <Typography gutterBottom variant="h5" component="div">
                         {props.entry.name}

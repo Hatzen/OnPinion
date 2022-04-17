c0fafc1f163806fe6bd894bab20dd53cd61f97e8
Hatzen <kaihartz@gmx.net>
2022-04-17 16:54:53 +0200

    added config to use relative imports

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 /* eslint-disable */ // TODO: Make it proper..
 
+// const dev = process.env.NODE_ENV === 'dev';
+// const prod = !dev; // Be more restrictive for dev environment (as firebase buildscripts might not set properly.)
+
 const prod = process.env.NODE_ENV === 'production';
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-
-
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
   mode: prod ? 'production' : 'development',
@@ -21,6 +23,9 @@ module.exports = {
         exclude: /node_modules/,
         resolve: {
           extensions: ['.ts', '.tsx', '.js', '.json'],
+          // Avoid relative imports like in tsconfig.
+          // https://www.npmjs.com/package/tsconfig-paths-webpack-plugin
+          plugins: [new TsconfigPathsPlugin({})],
         },
         use: 'ts-loader',
       },
@@ -38,6 +43,7 @@ module.exports = {
     // https://stackoverflow.com/questions/69901768/webpack-dev-server-enables-features-then-disconnects-twice-when-using-host-op
     allowedHosts: ['all']
   },
+  // Only create source maps in dev environment
   devtool: prod ? undefined : 'source-map',
   plugins: [
     new HtmlWebpackPlugin({
